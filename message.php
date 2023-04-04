54497d026f230848dc4e590c39ba9e223ec0ac92
hunter-dean <58790987+hunter-dean@users.noreply.github.com>
2023-04-04 08:59:13 -0400

    Create message.php

---
+++ message.php
@@ -0,0 +1,78 @@
+body {
+  font-family: Arial, sans-serif;
+  background-color: #c7d9ff;
+  color: #333;
+}
+
+header {
+  background-color: #336699;
+  color: white;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.logo img {
+  height: 50px;
+}
+
+nav ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+nav li {
+  float: left;
+}
+
+nav li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+nav li a:hover {
+  background-color: #003366;
+}
+
+main {
+  padding: 20px;
+  text-align: center;
+}
+
+h1 {
+  color: #336699;
+  font-size: 36px;
+  margin-top: 40px;
+}
+
+p {
+  line-height: 1.5;
+  margin-bottom: 20px;
+}
+
+button {
+  background-color: #336699;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  font-size: 18px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #003366;
+}
+
+footer {
+  background-color: #336699;
+  color: white;
+  padding: 10px;
+  text-align: center;
+}
