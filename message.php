f9382c9afce8dcf7a6c5d323652d6121e670f801
hunter-dean <58790987+hunter-dean@users.noreply.github.com>
2023-04-04 09:05:10 -0400

    Update message.php

--- message.php
+++ message.php
@@ -1,78 +1,33 @@
-body {
-  font-family: Arial, sans-serif;
-  background-color: #c7d9ff;
-  color: #333;
-}
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = trim($_POST["name"]);
+  $message = trim($_POST["message"]);
+  $to = "front.office@baxter-academy.net"; // replace with your email address
 
-header {
-  background-color: #336699;
-  color: white;
-  padding: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
+  if (!empty($name) && !empty($message)) {
+    $subject = "New message from $name";
+    $body = "Name: $name\n\nMessage: $message";
+    $headers = "From: $name <$to>\r\n" .
+               "Reply-To: $name <$to>\r\n" .
+               "X-Mailer: PHP/" . phpversion();
 
-.logo img {
-  height: 50px;
+    if (mail($to, $subject, $body, $headers)) {
+      echo "<p>Thank you for your message, $name!</p>";
+    } else {
+      echo "<p>Sorry, there was a problem sending your message. Please try again later.</p>";
+    }
+  } else {
+    echo "<p>Please enter both your name and message.</p>";
+  }
 }
+?>
 
-nav ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-}
-
-nav li {
-  float: left;
-}
-
-nav li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-nav li a:hover {
-  background-color: #003366;
-}
-
-main {
-  padding: 20px;
-  text-align: center;
-}
-
-h1 {
-  color: #336699;
-  font-size: 36px;
-  margin-top: 40px;
-}
-
-p {
-  line-height: 1.5;
-  margin-bottom: 20px;
-}
-
-button {
-  background-color: #336699;
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  border-radius: 5px;
-  font-size: 18px;
-  cursor: pointer;
-}
-
-button:hover {
-  background-color: #003366;
-}
-
-footer {
-  background-color: #336699;
-  color: white;
-  padding: 10px;
-  text-align: center;
-}
+<form method="post">
+  <label for="name">Name:</label>
+  <input type="text" name="name" id="name" required>
+  <br>
+  <label for="message">Message:</label>
+  <textarea name="message" id="message" rows="5" required></textarea>
+  <br>
+  <button type="submit">Send Message</button>
+</form>
