6ab03329f0c4f9c35f67b0d0b401b1ca6db54bed
hunter-dean <58790987+hunter-dean@users.noreply.github.com>
2023-11-20 23:55:10 -0500

    Update script.js

--- shrek/script.js
+++ shrek/script.js
@@ -1,20 +1,26 @@
-// Get the header element
-const header = document.querySelector('header h1');
+// Wrap your existing JavaScript code in a jQuery ready function
+$(document).ready(function () {
+    // Get the header element
+    const header = $('header h1');
 
-// Function to generate a random color
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+    // Function to generate a random color
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
     }
-    return color;
-}
 
-// Function to change the header text color
-function changeHeaderColor() {
-    header.style.color = getRandomColor();
-}
+    // Function to change the header text color
+    function changeHeaderColor() {
+        header.css('color', getRandomColor());
+    }
 
-// Change the header text color every 2 seconds
-setInterval(changeHeaderColor, 2000);
+    // Change the header text color every 2 seconds
+    setInterval(changeHeaderColor, 2000);
+
+    // Shake the "Thanks for visiting!" text when the page is loaded
+    $('p:contains("Thanks for visiting!")').effect('shake', { distance: 10, times: 3 }, 1000);
+});
