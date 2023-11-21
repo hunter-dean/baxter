66e9060ea7e915ab4b7db8e930cca8cd9df4ff92
hunter-dean <58790987+hunter-dean@users.noreply.github.com>
2023-11-21 11:28:15 -0500

    Update kfc.js

--- funny/kfc.js
+++ funny/kfc.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Welcome to KFC! What would you like to order?");
+
+    // Function to simulate placing an order
+    function placeOrder() {
+        var orderConfirmation = confirm("Do you want to place an order?");
+        
+        if (orderConfirmation) {
+            alert("Your order has been placed! Thank you for choosing KFC.");
+        } else {
+            alert("Feel free to explore the menu and place an order when you're ready.");
+        }
+    }
+
+    // Attach the placeOrder function to a button click event
+    var orderButton = document.getElementById("place-order-btn");
+
+    if (orderButton) {
+        orderButton.addEventListener("click", placeOrder);
+    }
+});
 
